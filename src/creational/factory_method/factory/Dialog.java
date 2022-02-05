9ae78ffae0ba919f9feabef636e03c507c61b2e7
Luis Fernando Cezar dos Santos <nandocezar22@gmail.com>
2022-02-05 11:41:20 -0300

    insert design pattern factory method

---
+++ src/creational/factory_method/factory/Dialog.java
@@ -0,0 +1,19 @@
+package creational.factory_method.factory;
+
+import creational.factory_method.buttons.Button;
+
+public abstract class Dialog {
+
+    public void renderWindow() {
+        // ... other code ...
+
+        Button okButton = createButton();
+        okButton.render();
+    }
+
+    /**
+     * Subclasses will override this method in order to create specific button
+     * objects.
+     */
+    public abstract Button createButton();
+}
---
+++ src/creational/factory_method/factory/WindowsDialog.java
@@ -0,0 +1,12 @@
+package creational.factory_method.factory;
+
+import creational.factory_method.buttons.Button;
+import creational.factory_method.buttons.WindowsButton;
+
+public class WindowsDialog extends Dialog {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+}
