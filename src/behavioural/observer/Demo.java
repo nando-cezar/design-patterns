d8f7513c3329f487573dda95b2bcd740b6976fc9
Luis Fernando Cezar dos Santos <nandocezar22@gmail.com>
2022-02-05 16:15:58 -0300

    insert project observer

---
+++ src/behavioural/observer/Demo.java
@@ -0,0 +1,20 @@
+package behavioural.observer;
+
+import behavioural.observer.editor.Editor;
+import behavioural.observer.listeners.EmailNotificationListener;
+import behavioural.observer.listeners.LogOpenListener;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+
+        try {
+            editor.openFile("test.txt");
+            editor.saveFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/behavioural/observer/listeners/EventListener.java
@@ -0,0 +1,7 @@
+package behavioural.observer.listeners;
+
+import java.io.File;
+
+public interface EventListener {
+    void update(String eventType, File file);
+}
