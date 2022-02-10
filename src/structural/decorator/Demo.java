e8d04d906bc8e1f4aafeedeb11d909656602bcd7
Luis Fernando Cezar dos Santos <nandocezar22@gmail.com>
2022-02-10 18:14:11 -0300

    insert design pattern adapter and decorator

---
+++ src/structural/decorator/Demo.java
@@ -0,0 +1,25 @@
+package structural.decorator;
+
+import structural.decorator.decorators.CompressionDecorator;
+import structural.decorator.decorators.DataSource;
+import structural.decorator.decorators.DataSourceDecorator;
+import structural.decorator.decorators.EncryptionDecorator;
+import structural.decorator.decorators.FileDataSource;
+
+public class Demo {
+    public static void main(String[] args) {
+        String salaryRecords = "Name,Salary\nJohn Smith,100000\nSteven Jobs,912000";
+        DataSourceDecorator encoded = new CompressionDecorator(
+                                         new EncryptionDecorator(
+                                             new FileDataSource("out/OutputDemo.txt")));
+        encoded.writeData(salaryRecords);
+        DataSource plain = new FileDataSource("out/OutputDemo.txt");
+
+        System.out.println("- Input ----------------");
+        System.out.println(salaryRecords);
+        System.out.println("- Encoded --------------");
+        System.out.println(plain.readData());
+        System.out.println("- Decoded --------------");
+        System.out.println(encoded.readData());
+    }
+}
